3408d866fe81dd2ad550cc0a561bc277bfde8151
hiendbui <hiendbui0@gmail.com>
2021-04-13 20:28:45 -0700

    Setup mongo db with keys

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-// const db = require("./config/keys").mongoURI;
-// const users = require("./routes/api/users");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
-const config = require('./config');
-const BoxSDK = require('box-node-sdk');
-const sdk = BoxSDK.getPreconfiguredInstance(config);
-// var authorize_url = sdk.getAuthorizeURL({
-// 	response_type: 'code'
-// });
-// console.log(authorize_url)
-const client = sdk.getAppAuthClient('user', '15794584626');
-// var client = sdk.getBasicClient('gZH3f1wduBDGdGylYy7olUzCFhv2dRQg');
 
-var fs = require('fs');
-var stream = fs.createReadStream('./HienBuiResume.pdf');
-client.folders.create('0', '0')
-  .then(folder => {console.log(folder)})
-  .catch(error => {  console.log(error) })
-
-// client.files.uploadFile('0', 'HienBuiResume.pdf', stream)
-// 	.then(fileObject => { console.log(fileObject) })
-// 	.catch(error => {  console.log(error) });
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
@@ -33,10 +15,10 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-// mongoose
-//     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => console.log("Connected to MongoDB successfully"))
-//     .catch(err => console.log(err));
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
 
 app.use(bodyParser.urlencoded({ 
     extended: false
@@ -44,9 +26,9 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 
-// app.use("/api/users", users);
+app.use("/api/users", users);
 app.use(passport.initialize());
-// require('./config/passport')(passport);
+require('./config/passport')(passport);
 
 const port = process.env.PORT || 5000;
 
