18d657300dc075806c1e4d94461ba5debc3b62c3
hiendbui <hiendbui0@gmail.com>
2021-04-15 15:32:03 -0700

    setup app and store

---
+++ frontend/src/components/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { AuthRoute, InstructorRoute } from '../util/route_util';
+import { Route, Switch } from 'react-router-dom';
+
+
+
+const App = () => (
+  <div>
+    <Switch>
+        {/* <Route exact path="/" component={MainPage} />*/}
+    </Switch>
+  </div>
+);
+
+export default App;
