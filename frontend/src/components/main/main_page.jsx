5348686bb04f0038d9657e092fc190ff890b4d13
hiendbui <hiendbui0@gmail.com>
2021-04-15 20:18:24 -0700

    Add main page w/ styling

---
+++ frontend/src/components/main/main_page.jsx
@@ -0,0 +1,37 @@
+import {React, useState} from 'react';
+import { useDispatch } from 'react-redux';
+import LoginForm from './login_form';
+import SignupForm from './signup_form';
+import { clearErrors } from '../../actions/session_actions';
+import LandingPage from "../../assets/images/landing_page.png";
+
+import './main.scss'
+
+function MainPage() {
+  const dispatch = useDispatch();
+
+  const [formType, toggleForm] = useState('login')
+  const otherFormType = formType === 'login' ? 'signup' : 'login';
+  const sessionForm = formType === 'login' ? <LoginForm/> : <SignupForm/>
+  const buttonValue = formType === 'login' ? 'Open an Account' : 'Log into Account'
+  
+  return (
+    <div className="main">
+        <img id='landing' src={LandingPage} />
+        <div className='session-form-container'>
+          {sessionForm}
+          <button className='toggle-btn' 
+            onClick={() => {
+              dispatch(clearErrors());
+              toggleForm(otherFormType);
+            }}
+          >
+            {buttonValue}
+          </button>
+        </div>
+    </div>
+  );
+  
+}
+
+export default MainPage;
