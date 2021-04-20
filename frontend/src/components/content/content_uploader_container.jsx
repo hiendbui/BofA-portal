43c84ff50a56663443d6f6a361dd23a416834429
hiendbui <hiendbui0@gmail.com>
2021-04-20 00:29:36 -0700

    Add loading gif for porta

--- frontend/src/components/content/content_uploader_container.jsx
+++ frontend/src/components/content/content_uploader_container.jsx
@@ -1,48 +1,57 @@
 import { React, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { IntlProvider } from 'react-intl';
-import { deleteFolder } from '../../actions/folder_actions';
+import { deleteFolder, removeFolderId } from '../../actions/folder_actions';
 import ContentUploader from 'box-ui-elements/es/elements/content-uploader';
 import 'box-ui-elements/dist/uploader.css';
 
 
 
-function ContentUpload({token,folderId,appType,fileType,nextFile,quit}) {
+function ContentUploaderContainer({token,folderId,appType,fileType,nextFile,changeAppType}) {
     const buttonName = fileType === "Bank Statements" ? 'Submit' : 'Next';
     const [buttonClass, toggleButton] = useState('disabled');
     const dispatch = useDispatch();
 
+    function deleteApplication() {
+        nextFile(-1);
+        changeAppType(null);
+        dispatch(deleteFolder(folderId));
+    }
+
         return (
-            <div className='content-uploader'>
+            <div className='content-uploader-container'>
                 <h1>{appType} Application</h1>
                 <br/>
                 <h2>Please upload your {fileType} below:</h2>
                 <br/>
                 <IntlProvider locale="en">
                     <ContentUploader
+                        className='content-uploader'
                         language='en-US'
                         rootFolderId={folderId?.toString()}
                         token={token}
                         logoUrl={'https://1000logos.net/wp-content/uploads/2016/10/Bank-of-America-logo.png'}
                         fileLimit={3}
-                        onClose={()=> {
-                            nextFile(-1);
-                            quit(null);
-                            dispatch(deleteFolder(folderId));
-                        }}
+                        onClose={deleteApplication}
                         onComplete={toggleButton}
                     />
                 </IntlProvider>
+                <button
+                    className='delete'
+                    onClick={deleteApplication}>
+                    Delete App
+                </button>
                 <button 
                     className={`next ${buttonClass}`} 
                     onClick={() => {
                         if (buttonName === 'Submit') {
                             nextFile(-1);
-                            quit(null);
+                            changeAppType(null);
+                            dispatch(removeFolderId());
                             return;
                         }
-                        toggleButton('disabled')
-                        nextFile(prevIdx=>prevIdx+1)
+                        toggleButton('disabled');
+                        nextFile(prevIdx=>prevIdx+1);
                     }}
                 >{buttonName}
                 </button>
@@ -51,4 +60,4 @@ function ContentUpload({token,folderId,appType,fileType,nextFile,quit}) {
 
 };
 
-export default ContentUpload;
+export default ContentUploaderContainer;
