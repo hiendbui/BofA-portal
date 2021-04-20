e284470b7f771daf42a629ad3f147c7e03bf73d4
hiendbui <hiendbui0@gmail.com>
2021-04-20 01:51:22 -0700

    Finish content explore

--- frontend/src/components/content/content_explore.jsx
+++ frontend/src/components/content/content_explore.jsx
@@ -1,17 +1,38 @@
-import { React } from 'react';
+import { React, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { IntlProvider } from 'react-intl';
-import { fetchToken } from '../../actions/session_actions'
+import { fetchToken, logout, clearErrors } from '../../actions/session_actions'
 import ContentExplorer from 'box-ui-elements/es/elements/content-explorer';
 import 'box-ui-elements/dist/explorer.css';
+import './content_explore.scss';
 
 
 
-function ContentExplore({token}) {
-    return (
-        <div>
+function ContentExplore() {
+    const dispatch = useDispatch();
+    useEffect(() => dispatch(fetchToken()),[]);
+   
+    const token = useSelector(state => state.session.token);
+    
+    if (token) {
+        return (
+        <div className='content-explore'>
+            <br/>
+            <div className='nav'>
+                Welcome, Admin
+                <button
+                    className='next signout'
+                    onClick={() => {
+                        dispatch(logout())
+                        dispatch(clearErrors())
+                    }}
+                >
+                    Sign Out
+                </button>
+            </div>
             <IntlProvider locale="en">
                 <ContentExplorer
+                    className='content-explorer'
                     contentPreviewProps={{
                         contentSidebarProps: {
                             detailsSidebarProps: {
@@ -28,13 +49,17 @@ function ContentExplore({token}) {
                             hasVersions: true,
                         },
                     }}
+                    canShare={false}
+                    canRename={false}
+                    canDelete={false}
+                    canDownload={false}
                     logoUrl={'https://1000logos.net/wp-content/uploads/2016/10/Bank-of-America-logo.png'}
                     rootFolderId={'0'}
                     token={token}
                 />
             </IntlProvider>
         </div>
-    )
+    )} else return null; 
 };
 
 export default ContentExplore;
